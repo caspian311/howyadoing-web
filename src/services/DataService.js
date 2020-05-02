90b63ceda2a8eeb04c48c02872d74b9a10557c21
Matt Todd <matt.c.todd@gmail.com>
2020-05-02 17:38:48 -0500

    formatting the data for display

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -8,7 +8,18 @@ class DataService {
         console.log('hitting: ', url);
         return axios.get(url)
             .then((data) => {
-                return data.data
+                const dateFormatter = new Intl.DateTimeFormat('en', { month: '2-digit', day: '2-digit' })
+
+                return data.data.map((datum) => {
+                    let parsedDate = Date.parse(datum.date)
+                    let formattedDate = dateFormatter.format(parsedDate)
+
+                    return {
+                        name: formattedDate,
+                        weight: datum.value,
+                        goal: datum.goal
+                    };
+                })
             })
             .catch((error) => {
                 console.log('error: ', error)
