f03e7b0e2fc856ddbb32008fc99c9e805f2a5d20
Matt Todd <matt.c.todd@gmail.com>
2020-05-02 18:11:21 -0500

    made the graph responsive

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Graph.css';
-import {LineChart, Legend, YAxis, XAxis, Tooltip, CartesianGrid, Line} from 'recharts';
+import {ResponsiveContainer, LineChart, Legend, YAxis, XAxis, Tooltip, CartesianGrid, Line} from 'recharts';
 import DataService from '../services/DataService'
 
 class Graph extends Component {
@@ -23,23 +23,23 @@ class Graph extends Component {
 
     render() {
         return (      
-            <LineChart
-                width={500}
-                height={300}
-                data={this.state.data}
-                margin={{
-                    top: 5, right: 30, left: 20, bottom: 5,
-                }}
-                className="Graph"
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis domain={[ 190, 250 ]} />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="weight" stroke="#8884d8" strokeWidth="3" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="goal" stroke="#82ca9d" strokeWidth="3" />
-            </LineChart>
+            <ResponsiveContainer width="100%" aspect={4.0/3.0}>
+                <LineChart
+                    data={this.state.data}
+                    margin={{
+                        top: 10, right: 10, left: 10, bottom: 10,
+                    }}
+                    className="Graph"
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis domain={[ 190, 250 ]} />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="weight" stroke="#8884d8" strokeWidth="3" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="goal" stroke="#82ca9d" strokeWidth="3" />
+                </LineChart>
+            </ResponsiveContainer>
         )
     }
 }
