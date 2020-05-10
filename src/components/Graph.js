27bd51186f919c4710ad139ad65137282e8f75db
Matt Todd <matt.c.todd@gmail.com>
2020-05-09 21:42:01 -0500

    was able to make grpah simplier since it's not fetching data anymore

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {ResponsiveContainer, LineChart, Legend, YAxis, XAxis, Tooltip, CartesianGrid, Line} from 'recharts';
 
 import './Graph.css';
 
-class Graph extends Component {
-    render() {
-        return (      
-            <ResponsiveContainer width="100%" aspect={4.0/3.0}>
-                <LineChart
-                    data={this.props.data}
-                    margin={{
-                        top: 10, right: 10, left: 10, bottom: 10,
-                    }}
-                    className="Graph"
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis domain={[ 190, 250 ]} />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="weight" stroke="#8884d8" strokeWidth="3" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="goal" stroke="#82ca9d" strokeWidth="3" />
-                </LineChart>
-            </ResponsiveContainer>
-        )
-    }
+function Graph(props)  {
+    return (      
+        <ResponsiveContainer width="100%" aspect={4.0/3.0}>
+            <LineChart
+                data={props.data}
+                margin={{
+                    top: 10, right: 10, left: 10, bottom: 10,
+                }}
+                className="Graph"
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis domain={[ 190, 250 ]} />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="weight" stroke="#8884d8" strokeWidth="3" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="goal" stroke="#82ca9d" strokeWidth="3" />
+            </LineChart>
+        </ResponsiveContainer>
+    )
 }
 
 export default Graph
